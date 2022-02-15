e7edc30538c262768e3a0873f1c7de3f9a0183a9
Lorena <lorena.del88@hotmail.com>
2022-02-15 21:21:31 +0100

    html y js

---
+++ main.js
@@ -0,0 +1,35 @@
+console.log("Estoy aprobado?")
+
+
+var name = prompt('¿cual es tu nombre?');
+var nota = prompt('¿Cual es tu nota?');
+
+
+var insuficiente = <=4
+var Sufiente = <=6
+var Notable = <=8
+var Sobresaliente = <=10
+
+var nota = 1;
+var nota = 2;
+var nota = 3;
+var nota = 4;
+var nota = 5;
+var nota = 6;
+var nota = 7;
+var nota = 8;
+var nota = 9;
+var nota = 10;
+
+do( nota <='4' )
+console.log("Insuficiente");
+
+do( nota <='6')
+console.log("Suficiente");
+
+do( nota <='8')
+console.log("Notable");
+
+do( nota <='10')
+console.log("Sobresaliente");
+
